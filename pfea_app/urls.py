b9ee04b605ec8bbf6ecd6563b6e390d0b2ee2ecb
Juan Samuel Vargas <juan.samuel.vargas@gmail.com>
2016-10-25 13:52:16 -0700

    Add Mision, vision y principios page

--- pfea_app/urls.py
+++ pfea_app/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url
 
-from .views import index
-# or from .views.index import IndexView
+# or from .views import index
+from .views.index import IndexView
+from .views.mision_vision_principios import MvpView
 
 app_name = 'pfea_app'
 urlpatterns = [
-    url(r'^$', index.IndexView.as_view(), name='index'),
+    url(r'^$', IndexView.as_view(), name='index'),
+    url(r'^mision-vision-principios$', MvpView.as_view(), name='mvp'),
 ]
