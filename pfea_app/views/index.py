c59af752793d9af52cf935a4e392a99ecca35ca6
Juan Samuel Vargas <juan.samuel.vargas@gmail.com>
2016-11-20 14:15:26 -0800

    Frontpage: News link to the news site instead of the google redirect link

--- pfea_app/views/index.py
+++ pfea_app/views/index.py
@@ -1,3 +1,5 @@
+import urllib.parse as urlparse
+
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
@@ -7,14 +9,19 @@ def index(request):
     context = {}
 
     feed = feedparser.parse('https://www.google.com/alerts/feeds/08631104230436444761/3823783356340969957')
-    # Las ultimas tres son las primeras tres en el arreglo
+    # Las tres mas recientes son las primeras tres en el arreglo
     last_three_entries = feed.entries[:3]
 
     context['news'] = []
     for entry in last_three_entries:
         entry_dict = {}
+
+        google_link = entry.link
+        parsed = urlparse.parse_qs(urlparse.urlparse(google_link).query)
+        link = parsed['url'][0]
+
         entry_dict['title'] = entry.title
-        entry_dict['link'] = entry.link
+        entry_dict['link'] = link
         context['news'].append(entry_dict)
 
     return render(request, 'home/index.html', context)
