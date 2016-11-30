d8eb86318bb32a7bb2f2c3ffde2b3e0570bc4d87
Juan Samuel Vargas <juan.samuel.vargas@gmail.com>
2016-11-30 15:42:38 -0800

    Herramienta: Add DeleteView, UpdateView and CreateView

--- pfea_app/urls.py
+++ pfea_app/urls.py
@@ -20,6 +20,9 @@ from .views.solicitud_borrar import SolicitudDeVoluntariosDelete
 from .views.solicitud_aceptar import solicitud_aceptar
 from .views.gracias import GraciasView
 from .views.herramientas import HerramientaList
+from .views.registrar_herramienta import HerramientaCreate
+from .views.borrar_herramienta import HerramientaDelete
+from .views.editar_herramienta import HerramientaUpdate
 from .views.libros import LibroList
 from .views.registrar_libro import LibroCreate
 from .views.borrar_libro import LibroDelete
@@ -47,15 +50,22 @@ urlpatterns = [
     # Apoyanos
     url(r'^donativos/$', DonativosView.as_view(), name='donativos'),
     url(r'^voluntarios/$', SolicitudDeVoluntariosCreate.as_view(), name='voluntarios'),
+    url(r'^gracias/$', GraciasView.as_view(), name='gracias'),
+
+    # Voluntarios
     url(r'^voluntarios/solicitudes/$', SolicitudDeVoluntariosList.as_view(), name='solicitudes'),
     url(r'^voluntarios/lista/$', VoluntarioList.as_view(), name='lista_voluntarios'),
     url(r'^voluntarios/solicitudes/(?P<pk>\d+)/$', SolicitudDeVoluntariosDetail.as_view(), name='solicitud'),
     url(r'^voluntarios/solicitudes/(?P<pk>\d+)/borrar/$', SolicitudDeVoluntariosDelete.as_view(), name='borrar_solicitud'),
     url(r'^voluntarios/solicitudes/(?P<pk>\d+)/aceptar/$', solicitud_aceptar, name='aceptar_solicitud'),
-    url(r'^gracias/$', GraciasView.as_view(), name='gracias'),
 
-    # Modelos
+    # Herramientas
     url(r'^herramientas/$', HerramientaList.as_view(), name='lista_herramientas'),
+    url(r'^herramientas/registrar/$', HerramientaCreate.as_view(), name='registrar_herramienta'),
+    url(r'^herramientas/(?P<pk>\d+)/borrar/$', HerramientaDelete.as_view(), name='borrar_herramienta'),
+    url(r'^herramientas/(?P<pk>\d+)/editar/$', HerramientaUpdate.as_view(), name='editar_herramienta'),
+
+    # Libros
     url(r'^libros/$', LibroList.as_view(), name='lista_libros'),
     url(r'^libros/registrar/$', LibroCreate.as_view(), name='registrar_libro'),
     url(r'^libros/(?P<pk>\d+)/borrar/$', LibroDelete.as_view(), name='borrar_libro'),
