c3b7d6b67d28e07adfad34c618d77f7bbff805d8
FranciscodaMVP <isc.valdezfrancisco@gmail.com>
2016-12-01 13:10:00 -0800

    merge master, change footer and index

--- pfea_app/admin.py
+++ pfea_app/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import (Herramienta, Libro,
-    Voluntario, BasuraRecogida)
+    Voluntario, BasuraRecogida)#, InventarioDeHerramientas
 
 # Deprecated, InventarioDeHerramientas
 
--- pfea_app/models/__init__.py
+++ pfea_app/models/__init__.py
@@ -1,5 +1,7 @@
 from .voluntario import Voluntario
 from .herramienta import Herramienta
-# from .inventario_de_herramientas import InventarioDeHerramientas
+
+#from .inventario_de_herramientas import InventarioDeHerramientas
+
 from .libro import Libro
 from .basura_recogida import BasuraRecogida
