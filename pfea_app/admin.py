d508c667bcda1d77560df98a585337a8b748baf0
FranciscodaMVP <isc.valdezfrancisco@gmail.com>
2016-12-01 00:59:32 -0800

    fix inventario de herramientas delete

--- pfea_app/admin.py
+++ pfea_app/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
-from .models import (Herramienta, InventarioDeHerramientas, Libro,
-    Voluntario, BasuraRecogida)
+from .models import (Herramienta, Libro,
+    Voluntario, BasuraRecogida)#, InventarioDeHerramientas
 
 # Register your models here.
 admin.site.register(Herramienta)
-admin.site.register(InventarioDeHerramientas)
+#admin.site.register(InventarioDeHerramientas)
 admin.site.register(Libro)
 admin.site.register(Voluntario)
 admin.site.register(BasuraRecogida)
--- pfea_app/models/__init__.py
+++ pfea_app/models/__init__.py
@@ -1,5 +1,5 @@
 from .voluntario import Voluntario
 from .herramienta import Herramienta
-from .inventario_de_herramientas import InventarioDeHerramientas
+#from .inventario_de_herramientas import InventarioDeHerramientas
 from .libro import Libro
 from .basura_recogida import BasuraRecogida
