a8be2964930ceafb3658fa4b2f742a5364c0f9d9
FranciscodaMVP <isc.valdezfrancisco@gmail.com>
2016-11-30 22:10:48 -0800

    make migrations for herramientas model, change admin and init

--- pfea_app/admin.py
+++ pfea_app/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
-from .models import (Herramienta, InventarioDeHerramientas, Libro,
+from .models import (Herramienta, Libro,
     Voluntario, BasuraRecogida)
+
+# Deprecated, InventarioDeHerramientas
 
 # Register your models here.
 admin.site.register(Herramienta)
-admin.site.register(InventarioDeHerramientas)
+#admin.site.register(InventarioDeHerramientas)
 admin.site.register(Libro)
 admin.site.register(Voluntario)
 admin.site.register(BasuraRecogida)
